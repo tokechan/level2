2b4a56265539c1dd905aab9eb734c69dd71ac66b
toketime <flatoke@gmail.com>
2025-09-09 22:04:21 +0900

    commonbutton create

---
+++ src/components/atoms/CommonButton/CommonButton.tsx
@@ -0,0 +1,16 @@
+import { FC, ComponentProps, ReactNode } from "react";
+import styles from "./style.module.css";
+
+type CommonButtonProps = ComponentProps<"button"> & {
+  children: ReactNode;
+};
+
+export const CommonButton: FC<CommonButtonProps> = ({ 
+  children, 
+  type,
+  onClick, 
+}) => {
+  <button className={styles.button} type={type} onClick={onClick}>
+    {children}
+  </button>
+};
